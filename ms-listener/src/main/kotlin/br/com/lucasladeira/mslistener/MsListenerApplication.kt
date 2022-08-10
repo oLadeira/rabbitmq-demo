66553fa54fc6e7ed5865dac571f9367e94f52541
Lucas Ladeira <lucas-ladeira@hotmail.com>
2022-08-10 19:08:59 -0300

    ms-listener

---
+++ ms-listener/src/main/kotlin/br/com/lucasladeira/mslistener/MsListenerApplication.kt
@@ -0,0 +1,11 @@
+package br.com.lucasladeira.mslistener
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class MsListenerApplication
+
+fun main(args: Array<String>) {
+	runApplication<MsListenerApplication>(*args)
+}
---
+++ ms-listener/src/test/kotlin/br/com/lucasladeira/mslistener/MsListenerApplicationTests.kt
@@ -0,0 +1,13 @@
+package br.com.lucasladeira.mslistener
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class MsListenerApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}

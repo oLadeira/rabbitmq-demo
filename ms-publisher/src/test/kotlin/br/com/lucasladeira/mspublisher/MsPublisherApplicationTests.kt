3382bbd4a864792453d7f337f49abff45959a39d
Lucas Ladeira <lucas-ladeira@hotmail.com>
2022-08-11 11:11:21 -0300

    ms-publisher

---
+++ ms-publisher/src/test/kotlin/br/com/lucasladeira/mspublisher/MsPublisherApplicationTests.kt
@@ -0,0 +1,13 @@
+package br.com.lucasladeira.mspublisher
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class MsPublisherApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
